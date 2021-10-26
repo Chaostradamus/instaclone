8366cc3188469de613a2d616da36a9f72faf8d09
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-10-26 03:40:37 -0400

    modal work

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,9 +6,10 @@ function signIn({ providers }) {
     <>
       <Header />
 
-      <div className="flex flex-col items-center justify-center
-       min-h-screen py-10 px-14 text-center">
-
+      <div
+        className="flex flex-col items-center justify-center
+       min-h-screen py-10 px-14 text-center"
+      >
         <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
         <p className="font-xs italic">not real....learning only, foigs</p>
 
@@ -17,7 +18,9 @@ function signIn({ providers }) {
             <div key={provider.name}>
               <button
                 className="p-3 bg-blue-500 rounded-lg text-white"
-                onClick={() => SignIntoProvider(provider.id)}
+                onClick={
+                  () => SignIntoProvider(provider.id, { callbackUrl: "./" })
+                }
               >
                 Sign in with {provider.name}
               </button>
